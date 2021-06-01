2fbb03196d769efdf89b54a7bcb26cb3a4509a8f
Marcus <marcus@hardt-it.de>
2021-06-01 13:56:19 +0200

    fix html

--- runner.sh
+++ runner.sh
@@ -24,10 +24,8 @@ FLAVOR_HTML_FILE="${METRICS_DIR}fla-${DATESTR}.html"
 
 ./collect-sla-metrics.py -o $QUOTA_MD_FILE -f $FLAVOR_MD_FILE
 
-#mv out.json $JSON_FILE
-#
-#./json-to-table.py --input $JSON_FILE > $QUOTA_MD_FILE
 
+# SLA / QUOTA
 cat $STYLE_FILE > $QUOTA_HTML_FILE
 echo "Last updated on ${HTML_DATESTR}<br/>" >> $QUOTA_HTML_FILE
 pandoc -f gfm -t html $QUOTA_MD_FILE >> $QUOTA_HTML_FILE
@@ -36,11 +34,16 @@ test -e ${METRICS_DIR}index.html && rm ${METRICS_DIR}index.html
 cp $QUOTA_HTML_FILE ${METRICS_DIR}index.html
 
 
+# FLA / FLAVOR
 cat $STYLE_FILE > $FLAVOR_HTML_FILE
 echo "Last updated on ${HTML_DATESTR}<br/>" >> $FLAVOR_HTML_FILE
 pandoc -f gfm -t html $FLAVOR_MD_FILE >> $FLAVOR_HTML_FILE
 
+test -e ${METRICS_DIR}fla-index.html && rm ${METRICS_DIR}fla-index.html
+cp $FLAVOR_HTML_FILE ${METRICS_DIR}fla-index.html
 
-rsync ${METRICS_DIR}sla*html ${METRICS_DIR}index.html $REMOTE
+
+# RSYNC
+rsync ${METRICS_DIR}sla*html ${METRICS_DIR}fla*html ${METRICS_DIR}index.html $REMOTE
 echo "done for $HTML_DATESTR"
 
