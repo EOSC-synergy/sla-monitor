1d7322d7e1e9eccb0c0d202456c0fd07126b2b3e
Marcus <marcus@hardt-it.de>
2021-06-01 13:06:00 +0200

    add support for flavors

--- collect-sla-metrics.py
+++ collect-sla-metrics.py
@@ -15,11 +15,92 @@ def jprint(jsondata, do_print=True):
         return ""
     return retval
 
+def write_json_output(data, filename):
+    '''write json output'''
+    json_file_name = filename.replace('.md', '.json')
+    formatted_json_result = jprint (data, do_print = False)
+    json_output=open(json_file_name, "w")
+    json_output.write(formatted_json_result)
+    json_output.close()
+
+def write_md_output_quota(data, filename, METRICS):
+    '''write markdown output'''
+    filename=filename
+    output=open(filename, "w")
+    for metric in METRICS:
+        errors=[]
+        # print (F"{metric}")
+        # Table header
+        output.write(F"| {metric} |")
+        # for vo in filter(synergy_filter, vos[site][:2]):
+        for vo in SYNERGY_VOS:
+            output.write(F" {vo.split('.')[0]} |")
+
+        output.write("\n|---|")
+        for vo in SYNERGY_VOS:
+            output.write(F"---|")
+        output.write("\n")
+
+        # Table body
+        for site in data:
+            output.write(F"| {site} |")
+            for vo in SYNERGY_VOS:
+                # print (F"{site:10} | {vo:23}")
+                try:
+                    value=data[site][vo][metric]
+                except KeyError:
+                    value=""
+                except TypeError:
+                    try:
+                        errors.append(F"Error {len(errors)+1}: {data[site][vo]}")
+                        value=F"Error {len(errors)})"
+                    except Exception:
+                        value="Total Mess"
+                output.write(F" {value} |")
+            output.write("\n")
+        output.write("\n")
+        if metric == "cores":
+            output.write("\n".join(errors))
+        output.write("\n")
+    output.close()
+
+def write_md_output_flavor(data, filename, METRICS):
+    '''write markdown output'''
+    output=open(filename, "w")
+    for site in data:
+        for vo in SYNERGY_VOS:
+            errors=[]
+            # Table header
+            try:
+                _ = data[site][vo][0]
+
+                output.write(F"| {site} / {vo.split('.')[0]} |")
+                for metric in METRICS:
+                    output.write(F" {metric} |")
+
+                output.write("\n|---|")
+                for metric in METRICS:
+                    output.write(F"---|")
+                output.write("\n")
+
+                # Table body
+                for flavor_index in range(0,len(data[site][vo])):
+                    output.write("| ")
+                    for metric in METRICS:
+                        output.write(F"| {data[site][vo][flavor_index][metric]}")
+                    output.write("|\n")
+                output.write("\n")
+            except KeyError:
+                # print (F"{vo} is not available for {site}")
+                pass
+    output.close
+
 
 def parseOptions():
     '''Parse the commandline options'''
     parser = argparse.ArgumentParser(description='collect-sla-statistics')
     parser.add_argument('-o', dest='output',    default='metrics/out.md')
+    parser.add_argument('-f', dest='flavor',    default='metrics/flavor.md')
     args = parser.parse_args()
     return args
 
@@ -57,57 +138,34 @@ site_label = "Site"
 vo_label="VO"
 error_code_label="error_code"
 
-command = ("quota", "show")
-results = {}
-print (F"\n{site_label:14} | {vo_label:23} | {error_code_label}")
+quota_command = ("quota", "show")
+quotas = {}
+flavor_command = ("flavor", "list")
+flavors = {}
+print (F"\n{site_label:14} | {vo_label:23} | {error_code_label} | Type")
 for site in sites:
-    results[site]={}
+    quotas[site]={}
+    flavors[site]={}
     for vo in filter(synergy_filter, vos[site]):
-        error_code, result = fc_openstack(access_tokens[SYNERGY_VOS[vo]], site, vo, command)
-        results[site][vo] = result
-        print (F"{site:14} | {vo:23} | {error_code}")
+        error_code, result = fc_openstack(access_tokens[SYNERGY_VOS[vo]], site, vo, quota_command)
+        quotas[site][vo] = result
+        print (F"{site:14} | {vo:23} | {error_code:10} | q")
+        error_code, flavor = fc_openstack(access_tokens[SYNERGY_VOS[vo]], site, vo, flavor_command)
+        flavors[site][vo] = flavor
+        print (F"{site:14} | {vo:23} | {error_code:10} |   f")
 
-# jprint (results)
+# Write json output
+
+write_json_output(quotas, args.output)
+write_json_output(flavors, args.flavor)
+
+# json_input = open('flavor.json', 'r')
+# flavors = json.loads(json_input.read())
+# json_input.close()
 
 # Output loop
 METRICS=['cores', 'ram', 'instances', 'gigabytes', 'floating-ips']
 
+write_md_output_quota(quotas, args.output, METRICS)
+write_md_output_flavor(flavors, args.flavor, ['Name', 'RAM', 'Disk', 'Ephemeral', 'VCPUs', 'Is Public', 'ID'])
 
-
-output=open(args.output, "w")
-for metric in METRICS:
-    errors=[]
-    # print (F"{metric}")
-    # Table header
-    output.write(F"| {metric} |")
-    # for vo in filter(synergy_filter, vos[site][:2]):
-    for vo in SYNERGY_VOS:
-        output.write(F" {vo.split('.')[0]} |")
-
-    output.write("\n|---|")
-    for vo in SYNERGY_VOS:
-        output.write(F"---|")
-    output.write("\n")
-
-    # Table body
-    for site in results:
-        output.write(F"| {site} |")
-        for vo in SYNERGY_VOS:
-            # print (F"{site:10} | {vo:23}")
-            try:
-                value=results[site][vo][metric]
-            except KeyError:
-                value=""
-            except TypeError:
-                try:
-                    errors.append(F"Error {len(errors)+1}: {results[site][vo]}")
-                    value=F"Error {len(errors)})"
-                except Exception:
-                    value="Total Mess"
-            output.write(F" {value} |")
-        output.write("\n")
-    output.write("\n")
-    if metric == "cores":
-        output.write("\n".join(errors))
-    output.write("\n")
-output.close()
--- runner.sh
+++ runner.sh
@@ -12,23 +12,35 @@ CUR_HOUR=`date +%H`
 CUR_MIN=`date +%M`
 
 DATESTR=${CUR_YEAR}${CUR_MONTH}${CUR_DAY}-${CUR_HOUR}${CUR_MIN}
-
-JSON_FILE="${METRICS_DIR}sla-${DATESTR}.json"
-MD_FILE="${METRICS_DIR}sla-${DATESTR}.md"
-HTML_FILE="${METRICS_DIR}sla-${DATESTR}.html"
+HTML_DATESTR="${CUR_DAY}.${CUR_MONTH}.${CUR_YEAR} ${CUR_HOUR}:${CUR_MIN}"
 
 
-./collect-sla-metrics.py -o $MD_FILE
+#JSON_FILE="${METRICS_DIR}sla-${DATESTR}.json"
+QUOTA_MD_FILE="${METRICS_DIR}sla-${DATESTR}.md"
+QUOTA_HTML_FILE="${METRICS_DIR}sla-${DATESTR}.html"
+FLAVOR_MD_FILE="${METRICS_DIR}fla-${DATESTR}.md"
+FLAVOR_HTML_FILE="${METRICS_DIR}fla-${DATESTR}.html"
+
+
+./collect-sla-metrics.py -o $QUOTA_MD_FILE -f $FLAVOR_MD_FILE
 
 #mv out.json $JSON_FILE
 #
-#./json-to-table.py --input $JSON_FILE > $MD_FILE
+#./json-to-table.py --input $JSON_FILE > $QUOTA_MD_FILE
 
-cat $STYLE_FILE > $HTML_FILE
-pandoc -f gfm -t html $MD_FILE >> $HTML_FILE
+cat $STYLE_FILE > $QUOTA_HTML_FILE
+echo "Last updated on ${HTML_DATESTR}<br/>" >> $QUOTA_HTML_FILE
+pandoc -f gfm -t html $QUOTA_MD_FILE >> $QUOTA_HTML_FILE
 
 test -e ${METRICS_DIR}index.html && rm ${METRICS_DIR}index.html
-cp $HTML_FILE ${METRICS_DIR}index.html
+cp $QUOTA_HTML_FILE ${METRICS_DIR}index.html
+
+
+cat $STYLE_FILE > $FLAVOR_HTML_FILE
+echo "Last updated on ${HTML_DATESTR}<br/>" >> $FLAVOR_HTML_FILE
+pandoc -f gfm -t html $FLAVOR_MD_FILE >> $FLAVOR_HTML_FILE
+
 
 rsync ${METRICS_DIR}sla*html ${METRICS_DIR}index.html $REMOTE
+echo "done for $HTML_DATESTR"
 
